b53b3c442f4c73929fe569fea18fd7e3cdf62e7a
unknown <vinci22>
2024-05-13 19:57:58 -0500

    added frist iteration evaluations of concepts

---
+++ from_text_to_speech/app.py
@@ -0,0 +1,6 @@
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech
+
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+
+inputs = processor(text="Don't count the days, make the days count.", return_tensors="pt")
